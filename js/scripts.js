f74036a21558a4ed5a34fa725fe62d1ae31544f9
Matt <msmith4810@gmail.com>
2018-09-01 12:05:51 +1000

    removed redundant js

--- js/scripts.js
+++ js/scripts.js
@@ -86,28 +86,28 @@ AOS.init({
 // })
 
 // Start progress bar //
-$(window).load(function(){
-  $(window).scroll(function() {
-    var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
-    console.log(wintop);
-    var totalScroll = (wintop/(docheight-winheigt))*100;
-    console.log("total scroll" + totalScroll);
-    $(".KW_progressBar").css("width",totalScroll+"%");
-  });
+// $(window).load(function(){
+//   $(window).scroll(function() {
+//     var wintop = $(window).scrollTop(), docheight = $('body').height(), winheight = $(window).height();
+//     console.log(wintop);
+//     var totalScroll = (wintop/(docheight-winheigt))*100;
+//     console.log("total scroll" + totalScroll);
+//     $(".KW_progressBar").css("width",totalScroll+"%");
+//   });
 
-});
+// });
 // End progress bar //
 
 
 
 // Init controller
-var controller = new ScrollMagic.Controller({
-  globalSceneOptions: {
-    duration: $('section').height(),
-    triggerHook: .025,
-    reverse: true
-  }
-});
+// var controller = new ScrollMagic.Controller({
+//   globalSceneOptions: {
+//     duration: $('section').height(),
+//     triggerHook: .025,
+//     reverse: true
+//   }
+// });
 
 
 /*
@@ -120,73 +120,73 @@ scene_object = {
   }
 }
 */
-var scenes = {
-  'intro': {
-    'intro': 'intro-anchor'
-  },
-  'scene2': {
-    'section-1': 'anchor1'
-  },
-  'scene3': {
-    'section-2': 'anchor2'
-  },
-  'scene4': {
-    'section-3': 'anchor3'
-  }
-}
+// var scenes = {
+//   'intro': {
+//     'intro': 'intro-anchor'
+//   },
+//   'scene2': {
+//     'section-1': 'anchor1'
+//   },
+//   'scene3': {
+//     'section-2': 'anchor2'
+//   },
+//   'scene4': {
+//     'section-3': 'anchor3'
+//   }
+// }
 
-for(var key in scenes) {
-  // skip loop if the property is from prototype
-  if (!scenes.hasOwnProperty(key)) continue;
+// for(var key in scenes) {
+//   // skip loop if the property is from prototype
+//   if (!scenes.hasOwnProperty(key)) continue;
 
-  var obj = scenes[key];
+//   var obj = scenes[key];
 
-  for (var prop in obj) {
-    // skip loop if the property is from prototype
-    if(!obj.hasOwnProperty(prop)) continue;
+//   for (var prop in obj) {
+//     // skip loop if the property is from prototype
+//     if(!obj.hasOwnProperty(prop)) continue;
 
-    new ScrollMagic.Scene({ triggerElement: '#' + prop })
-        .setClassToggle('#' + obj[prop], 'active')
-        .addTo(controller);
-  }
-}
+//     new ScrollMagic.Scene({ triggerElement: '#' + prop })
+//         .setClassToggle('#' + obj[prop], 'active')
+//         .addTo(controller);
+//   }
+// }
 
 
 // Change behaviour of controller
 // to animate scroll instead of jump
-controller.scrollTo(function(target) {
+// controller.scrollTo(function(target) {
 
-  TweenMax.to(window, 0.5, {
-    scrollTo : {
-      y : target,
-      autoKill : true // Allow scroll position to change outside itself
-    },
-    ease : Cubic.easeInOut
-  });
+//   TweenMax.to(window, 0.5, {
+//     scrollTo : {
+//       y : target,
+//       autoKill : true // Allow scroll position to change outside itself
+//     },
+//     ease : Cubic.easeInOut
+//   });
 
-});
+// });
 
 
 //  Bind scroll to anchor links using Vanilla JavaScript
-var anchor_nav = document.querySelector('.anchor-nav');
+// var anchor_nav = document.querySelector('.anchor-nav');
 
-anchor_nav.addEventListener('click', function(e) {
-  var target = e.target,
-      id     = target.getAttribute('href');
+// anchor_nav.addEventListener('click', function(e) {
+//   var target = e.target,
+//       id     = target.getAttribute('href');
 
-  if(id !== null) {
-    if(id.length > 0) {
-      e.preventDefault();
-      controller.scrollTo(id);
+//   if(id !== null) {
+//     if(id.length > 0) {
+//       e.preventDefault();
+//       controller.scrollTo(id);
 
-      if(window.history && window.history.pushState) {
-        history.pushState("", document.title, id);
-      }
-    }
-  }
-});
+//       if(window.history && window.history.pushState) {
+//         history.pushState("", document.title, id);
+//       }
+//     }
+//   }
+// });
 
- AOS.init();
+//  AOS.init();
 
 /* 
  * Bind scroll to anchor links using jQuery
